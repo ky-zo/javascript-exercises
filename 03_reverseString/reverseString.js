d5a6b1a12de00b9a4ba2e21e5fc8ba57f89dff29
ky-zo <zowczak.kamil@gmail.com>
2023-02-25 14:00:59 +0100

    03 Done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let letters = text.split('').reverse().join('')
+    return letters
 };
 
 // Do not edit below this line
