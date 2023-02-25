79b4a1eb150bbc9509a336a5eb2c3976f68729fc
ky-zo <zowczak.kamil@gmail.com>
2023-02-25 13:37:24 +0100

    02 Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let result = ''
+    if( num < 0 ) {
+        result = 'ERROR'        
+    } else if (num === 0) {
+        result = ''
+    } else {
+        for (i=0;i<num;i++) {
+        result = result + string
+        }
+    }
+    return result
 };
 
 // Do not edit below this line
