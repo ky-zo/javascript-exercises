613bf197dbd5d4ea233b373d6d93471b6df61eb1
ky-zo <zowczak.kamil@gmail.com>
2023-02-26 12:12:26 +0100

    09 Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let letters = string.replace(/[^A-Za-z0-9]/g, '').toLowerCase().split('')
+    let lettersLength = letters.length
+    let upperLetter = ''
+    let lowerLetter = ''
+    let middleLetter = 0
+    let result = true
+    if (lettersLength%2 === 0) { // No. of letters divisible by 2
+        for (i = 0; i < lettersLength/2 && result === true; i++){
+            middleLetter = lettersLength/2           
+            upperLetter = letters[i];
+            lowerLetter = letters[lettersLength-1-i];
+            upperLetter === lowerLetter ? true : result = false
+        }
+    } else { // No. of letters not divisible by 2
+        for (i = 0; i != lettersLength-1-i && result === true; i++){
+            middleLetter = Math.floor(lettersLength/2);
+            upperLetter = letters[i];
+            lowerLetter = letters[lettersLength-1-i];
+            upperLetter === lowerLetter ? true : result = false
+            }
+    }
+    
+    return result 
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.spec.js
+++ 09_palindromes/palindromes.spec.js
@@ -4,19 +4,25 @@ describe('palindromes', () => {
   test('works with single words', () => {
     expect(palindromes('racecar')).toBe(true);
   });
-  test.skip('works with punctuation ', () => {
+  test('works on non-palindrome word with 8 letters', () => {
+    expect(palindromes('racetrac')).toBe(false);
+  });
+  test('works on non-palindrome words', () => {
+    expect(palindromes('racetrack')).toBe(false);
+  });
+  test('works with punctuation ', () => {
     expect(palindromes('racecar!')).toBe(true);
   });
-  test.skip('works with upper-case letters ', () => {
+  test('works with upper-case letters ', () => {
     expect(palindromes('Racecar!')).toBe(true);
   });
-  test.skip('works with multiple words', () => {
+  test('works with multiple words', () => {
     expect(palindromes('A car, a man, a maraca.')).toBe(true);
   });
-  test.skip('works with multiple words', () => {
+  test('works with multiple words', () => {
     expect(palindromes('Animal loots foliated detail of stool lamina.')).toBe(true);
   });
-  test.skip('doesn\'t just always return true', () => {
+  test('doesn\'t just always return true', () => {
     expect(palindromes('ZZZZ car, a man, a maracaz.')).toBe(false);
   });
 });
