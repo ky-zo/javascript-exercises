ab9c132b83bee818e87e176b12b55c78671b5818
ky-zo <zowczak.kamil@gmail.com>
2023-02-26 10:15:38 +0100

    Part of 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,17 +1,33 @@
-const add = function() {
-	
+const add = function(...numbers) {
+  let result = numbers[0];
+  for(i = 1; i<numbers.length; i++) {
+    result += Number(numbers[i])
+  }
+  return result
 };
 
-const subtract = function() {
-	
+const subtract = function(...numbers) {
+  let result = numbers[0];
+  for(i = 1; i<numbers.length; i++) {
+    result -= Number(numbers[i])
+  }
+  return result
 };
 
-const sum = function() {
-	
+const sum = function(set) {
+	let result = 0
+  for(i = 0; i<set.length; i++) {
+    result += Number(set[i])
+  }
+  return result
 };
 
 const multiply = function() {
-
+	let result
+  for(i = 1; i<set.length; i++) {
+    result += Number(set[i])
+  }
+  return toString(result)
 };
 
 const power = function() {
