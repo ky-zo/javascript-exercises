b707371f31a2870619e2d429bf2d99b44efa9d60
ky-zo <zowczak.kamil@gmail.com>
2023-02-28 11:03:24 +0100

    13 Done, ugly code but got it done

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,62 +1,31 @@
-//  const caesar = function(string,factor) {
-//     let arrayString = string.split('')
-//     let arrayStringCharCode = []
-//     let arrayCaesar = []
-
-//     //Create an array of unicode values for all the letters
-//     for (i = 0 ; i < arrayString.length ; i++){
-//         arrayStringCharCode.push(arrayString[i].charCodeAt())
-//     }
-
-//     //Loop through all the letters within CharCode Array
-//     for (i = 0 ; i < arrayStringCharCode.length ; i++) {
-//         arrayStringCharCode[i] = arrayStringCharCode[i] + factor
-//     }
-
-//     for (i = 0 ; i < arrayStringCharCode.length ; i++) {
-//         arrayStringCharCode[i] = String.fromCharCode(arrayStringCharCode[i])
-//     }
-
-
-
-//     return arrayStringCharCode
-
-//  };
-
-// // Do not edit below this line
-module.exports = caesar;
-
-
-let string = 'Hello, World!'
-const factor = 29;
-console.log('test')
-
-function caesar(string,factor) { 
+ const caesar = function(string,factor) {
+    //Divide the string into Array
     let arrayString = string.split('')
     let arrayStringCharCode = []
-    let loopRem = factor%26 === 0 ? 0 : 1
-    let loopFactor = Math.floor(factor/26) + loopRem
+
+    //Calculate the real factor by removing the full loops
+    factor = factor - (26 * ((factor/26)>>0))
 
     //Create an array of unicode values for all the letters
     for (i = 0 ; i < arrayString.length ; i++){
-        arrayStringCharCode.git push(arrayString[i].charCodeAt())
+        arrayStringCharCode.push(arrayString[i].charCodeAt())
     }
 
     //Loop through all the letters within CharCode Array
     for (i = 0 ; i < arrayStringCharCode.length ; i++) {
         if(arrayStringCharCode[i] >= 65 && arrayStringCharCode[i] <= 91){
             if(arrayStringCharCode[i] + factor >90){
-                arrayStringCharCode[i] = arrayStringCharCode[i] + factor - (26*loopFactor)
+                arrayStringCharCode[i] = arrayStringCharCode[i] + factor - 26
             } else if (arrayStringCharCode[i] + factor <65){
-                arrayStringCharCode[i] = arrayStringCharCode[i] + factor + (26*loopFactor)
+                arrayStringCharCode[i] = arrayStringCharCode[i] + factor + 26
             } else {
                 arrayStringCharCode[i] = arrayStringCharCode[i] + factor
             }
         } else if (arrayStringCharCode[i] >= 97 && arrayStringCharCode[i] <= 122){
             if(arrayStringCharCode[i] + factor >122){
-                arrayStringCharCode[i] = arrayStringCharCode[i] + factor - (26*loopFactor)
+                arrayStringCharCode[i] = arrayStringCharCode[i] + factor - 26
             } else if (arrayStringCharCode[i] + factor <97) {
-                arrayStringCharCode[i] = arrayStringCharCode[i] + factor + (26*loopFactor)
+                arrayStringCharCode[i] = arrayStringCharCode[i] + factor + 26
             } else {
                 arrayStringCharCode[i] = arrayStringCharCode[i] + factor
             }
@@ -69,5 +38,55 @@ function caesar(string,factor) {
         arrayStringCharCode[i] = String.fromCharCode(arrayStringCharCode[i])
     }
     return arrayStringCharCode.join('')
-}
-console.log(caesar(string,factor))
+};
+
+// // Do not edit below this line
+module.exports = caesar;
+
+
+// let string = 'Hello, World!'
+// const factor = -29;
+// console.log('test')
+
+// function caesar(string,factor) { 
+//     //Divide the string into Array
+//     let arrayString = string.split('')
+//     let arrayStringCharCode = []
+
+//     //Calculate the real factor by removing the full loops
+//     factor = factor - (26 * ((factor/26)>>0))
+
+//     //Create an array of unicode values for all the letters
+//     for (i = 0 ; i < arrayString.length ; i++){
+//         arrayStringCharCode.push(arrayString[i].charCodeAt())
+//     }
+
+//     //Loop through all the letters within CharCode Array
+//     for (i = 0 ; i < arrayStringCharCode.length ; i++) {
+//         if(arrayStringCharCode[i] >= 65 && arrayStringCharCode[i] <= 91){
+//             if(arrayStringCharCode[i] + factor >90){
+//                 arrayStringCharCode[i] = arrayStringCharCode[i] + factor - 26
+//             } else if (arrayStringCharCode[i] + factor <65){
+//                 arrayStringCharCode[i] = arrayStringCharCode[i] + factor + 26
+//             } else {
+//                 arrayStringCharCode[i] = arrayStringCharCode[i] + factor
+//             }
+//         } else if (arrayStringCharCode[i] >= 97 && arrayStringCharCode[i] <= 122){
+//             if(arrayStringCharCode[i] + factor >122){
+//                 arrayStringCharCode[i] = arrayStringCharCode[i] + factor - 26
+//             } else if (arrayStringCharCode[i] + factor <97) {
+//                 arrayStringCharCode[i] = arrayStringCharCode[i] + factor + 26
+//             } else {
+//                 arrayStringCharCode[i] = arrayStringCharCode[i] + factor
+//             }
+
+//         }
+// //        arrayStringCharCode[i] = arrayStringCharCode[i] + factor
+//     }
+
+//     for (i = 0 ; i < arrayStringCharCode.length ; i++) {
+//         arrayStringCharCode[i] = String.fromCharCode(arrayStringCharCode[i])
+//     }
+//     return arrayStringCharCode.join('')
+// }
+// console.log(caesar(string,factor))
