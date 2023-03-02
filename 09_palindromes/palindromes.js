a5316d80ad277e608dc8d2ed638c563eb04e1a87
ky-zo <zowczak.kamil@gmail.com>
2023-03-02 17:36:27 +0100

    Fixed Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,27 +1,28 @@
 const palindromes = function (string) {
     let letters = string.replace(/[^A-Za-z0-9]/g, '').toLowerCase().split('')
-    let lettersLength = letters.length
-    let upperLetter = ''
-    let lowerLetter = ''
-    let middleLetter = 0
-    let result = true
-    if (lettersLength%2 === 0) { // No. of letters divisible by 2
-        for (i = 0; i < lettersLength/2 && result === true; i++){
-            middleLetter = lettersLength/2           
-            upperLetter = letters[i];
-            lowerLetter = letters[lettersLength-1-i];
-            upperLetter === lowerLetter ? true : result = false
-        }
-    } else { // No. of letters not divisible by 2
-        for (i = 0; i != lettersLength-1-i && result === true; i++){
-            middleLetter = Math.floor(lettersLength/2);
-            upperLetter = letters[i];
-            lowerLetter = letters[lettersLength-1-i];
-            upperLetter === lowerLetter ? true : result = false
-            }
-    }
+    return (letters.join('') === letters.reverse().join(''))
+    // let lettersLength = letters.length
+    // let upperLetter = ''
+    // let lowerLetter = ''
+    // let middleLetter = 0
+    // let result = true
+    // if (lettersLength%2 === 0) { // No. of letters divisible by 2
+    //     for (i = 0; i < lettersLength/2 && result === true; i++){
+    //         middleLetter = lettersLength/2           
+    //         upperLetter = letters[i];
+    //         lowerLetter = letters[lettersLength-1-i];
+    //         upperLetter === lowerLetter ? true : result = false
+    //     }
+    // } else { // No. of letters not divisible by 2
+    //     for (i = 0; i != lettersLength-1-i && result === true; i++){
+    //         middleLetter = Math.floor(lettersLength/2);
+    //         upperLetter = letters[i];
+    //         lowerLetter = letters[lettersLength-1-i];
+    //         upperLetter === lowerLetter ? true : result = false
+    //         }
+    // }
     
-    return result 
+    // return result 
 };
 
 // Do not edit below this line
