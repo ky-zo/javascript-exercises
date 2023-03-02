7f18785cbdc98b9dbdf318b68ee6a07277cf27c0
ky-zo <zowczak.kamil@gmail.com>
2023-03-02 17:23:07 +0100

    Caluclator fix

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,37 @@
 const add = function(...numbers) {
-  let result = numbers[0];
-  for(i = 1; i<numbers.length; i++) {
-    result += Number(numbers[i])
-  }
-  return result
+  return numbers.reduce((total, next) => total + next,0);
+  // let result = numbers[0];
+  // for(i = 1; i<numbers.length; i++) {
+  //   result += Number(numbers[i])
+  // }
+  // return result
 };
 
 const subtract = function(...numbers) {
-  let result = numbers[0];
-  for(i = 1; i<numbers.length; i++) {
-    result -= Number(numbers[i])
-  }
-  return result
+  return numbers.reduce((total, next) => total - next);
+  // let result = numbers[0];
+  // for(i = 1; i<numbers.length; i++) {
+  //   result -= Number(numbers[i])
+  // }
+  // return result
 };
 
-const sum = function(set) {
-	let result = 0
-  for(i = 0; i<set.length; i++) {
-    result += Number(set[i])
-  }
-  return result
+const sum = function(numbers) {
+  return numbers.reduce((total, next) => total + next,0);
+	// let result = 0
+  // for(i = 0; i<set.length; i++) {
+  //   result += Number(set[i])
+  // }
+  // return result
 };
 
-const multiply = function(set) {
-	let result = 1
-  for(i = 0; i < set.length; i++) {
-    result *= Number(set[i])
-  }
-  return result
+const multiply = function(numbers) {
+  return numbers.reduce((total, next) => total * next)
+	// let result = 1
+  // for(i = 0; i < set.length; i++) {
+  //   result *= Number(set[i])
+  // }
+  // return result
 };
 
 const power = function(a,b) {
